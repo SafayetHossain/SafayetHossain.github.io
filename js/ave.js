c4039479f9244d4398e64fdb959875ac95883f06
SafayetHossain <37340889+SafayetHossain@users.noreply.github.com>
2018-12-11 01:36:00 +0600

    ave

---
+++ js/ave.js
@@ -0,0 +1,27 @@
+ // image details
+$(".img-details").hide();
+$(".d-img-1").show();
+$(document).ready(function(){
+    $(".img-1").click(function(){
+        $(".d-img-1").slideToggle("slow");
+    });
+     $(".img-2").click(function(){
+        $(".d-img-2").slideToggle("slow");
+    });
+      $(".img-3").click(function(){
+        $(".d-img-3").slideToggle("slow");
+    });
+       $(".img-4").click(function(){
+        $(".d-img-4").slideToggle("slow");
+    });
+        $(".img-5").click(function(){
+        $(".d-img-5").slideToggle("slow");
+    });
+         $(".img-6").click(function(){
+        $(".d-img-6").slideToggle("slow");
+    });
+});
+
+// header font
+
+
